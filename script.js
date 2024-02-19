0af3f1a24e6f13f8ac263f060c732a5658058f9d
nurislam <github20243@gmail.com>
2024-02-19 13:06:41 +0300

    git hub

---
+++ script.js
@@ -0,0 +1,112 @@
+
+console.log("My name is");
+
+const firstName = 'Nurislam';
+
+console.log("Nurislam")
+
+const name = "My name is: Simon"
+
+console.log("My name is: Simon")
+
+const three = 3;
+const five = 5;
+const to = 2;
+
+let totalCost = three * five - five -to;
+console.log(`Total cost: $${totalCost}`);
+
+const dollar = 8;
+
+console.log(`Total cost: $${dollar}`);
+
+const number = 8;
+
+alert(`Total cost: $${number}`)
+
+let numbers = 590;
+let Aspire = 304;
+let fo = numbers + Aspire
+let fifti = 100;
+console.log(fo/fifti)
+
+let exic = 590;
+let menshe = 304;
+let puithon = exic + menshe;
+let makvin = 100;
+console.log(`Total cost: $${puithon / makvin}`)
+
+let van = 0.8;
+let tu = 2090;
+let sri = 2;
+let foti = 811;
+let fiveti = 100;
+let js = van * tu + sri * foti 
+let java =  fiveti;
+console.log(`items(4): ${js / fiveti}`)
+
+let miagi = 499 + 499;
+let paradise = miagi 
+let kossandra = 100;
+console.log(`Shipping & handling:$${paradise / kossandra}`)
+
+
+let matimka =  0.9 * 2095 + 2 * 799 + 466 + 460 
+
+let geometria = 100
+
+console.log(`$${matimka / geometria}`)
+
+
+let тринадцать = 2 * 2095 + 2 * 799 +499 + 499 * 0.1
+
+let slesh = 100
+
+console.log(`Estimated tax (10%): $${Math.round((2 * 2095 + 2 * 799 + 499 + 499) * 0.1) / 100}`);
+
+
+const result = "Simon"
+console.log(result);
+
+let nae = "Simon";
+console.log(`My name is: ${nae}`);
+
+let умножение = 5 + (2 * 3) + 5;
+console.log(умножение);
+
+let dolar = 16;
+console.log(`Cost of food: $${dolar}`);
+
+let nalog = 16;
+let tax = nalog * 0.1;
+console.log(tax)
+
+let sixteen = 16;
+let taxs = sixteen * 0.1;
+let  шестнадцать  = taxs + sixteen;
+console.log(шестнадцать);
+
+
+let seventeen = 17.6;
+console.log(`Total cost: ${seventeen}`);
+
+
+
+
+
+alert(`Total cost: $${puithon / makvin} Thank you, come again!`)
+
+const NurtName = prompt("Введите ваше имя");
+
+alert
+
+alert("Вы успешно зарегистрировались!");
+
+let isBoss = confirm("При перезагрузке сайта, введённые данные на форме будут очищены, Вы уверены что хотите перезагрузить?");
+
+
+let т = (2 * 2095) + 2 * 799 +499 + 499 * 0.1
+
+let s = 100
+
+console.log(`Estimated tax (10%): $${Math.round((2 * 2095 + 2 * 799 + 499 + 499) * 0.1) / 100}`);
